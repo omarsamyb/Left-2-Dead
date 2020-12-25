cb1d815323fd7f1f857c981d4f6c14f7a77541f5
B1aCkManTa <mohamed.mantawy@student.guc.edu.eg>
2020-12-26 00:32:02 +0200

    UI birth

---
+++ Assets/UI/PlayerInventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public InventoryObject inventory;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        print("innn");
+        var item = other.GetComponent<Item>();
+        if(item){
+            inventory.addItem(item.item, 1);
+            Destroy(other.gameObject);
+        }
+    }
+
+    // void OnApplicationQuit()
+    // {
+    //     inventory.container.Clear(); 
+    // }
+}
---
+++ Assets/UI/Scriptable Objects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Ingredient,
+    Craftable,
+    Ammo,
+    Weapon
+}
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemType type;
+
+}
