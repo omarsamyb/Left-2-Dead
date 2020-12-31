59c5abc8c73070cebda2fa4c3ac6e408c6ea5965
dondo98 <47148774+dondo98@users.noreply.github.com>
2020-12-31 23:11:44 +0200

    Horde
    add the horde prefab which is prefab attached to script to generate the horde

---
+++ Assets/Scripts/Zombies/HordeSpawner.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HordeSpawner : MonoBehaviour
+{
+	private int hordeCount;
+	private int spawnRatePerSec;
+	public GameObject enemyObj;
+	private bool isSpawning;
+	private bool fire;
+
+    void Update()
+	{
+		if (!fire) {
+			if (gameObject.tag == "Horde")
+			{
+				fire = true;
+				SetBasicHorde();
+			}
+			// true represent the pile bomb is attached to the player
+            else if (gameObject.tag == "Bomber" && true)
+			{
+				fire = true;
+				SetBomberHorde();
+            }
+		}
+		if(isSpawning)
+        {
+			isSpawning = false;
+			StartCoroutine(SpawnEnemy());
+		}
+	}
+	IEnumerator SpawnEnemy()
+	{
+		for (int i = 0; i <hordeCount ; i++)
+		{
+
+			Instantiate(enemyObj, transform.position, transform.rotation);
+			yield return new WaitForSeconds(1f / spawnRatePerSec);
+		}
+		isSpawning = false;
+
+		yield break;
+	}
+	public void SetBomberHorde()
+    {
+	isSpawning = true;
+	hordeCount = 16;
+	spawnRatePerSec = 4;
+	}
+	public void SetBasicHorde()
+    {
+	isSpawning = true;
+	hordeCount=20;
+	spawnRatePerSec=4;
+	}
+
+
+}
