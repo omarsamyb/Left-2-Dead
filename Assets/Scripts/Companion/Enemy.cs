166c5803b059d727763940e2149422cd1cb90f80
madoooaboelazaiem <madooo1998@hotmail.com>
2020-12-31 04:22:52 +0200

    Finished Tracking And Shooting Enemies

---
+++ Assets/Scripts/Companion/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+        {
+         if(col.gameObject.tag == "Bullet"){
+                Debug.Log("Hit");
+                Destroy(col.gameObject);
+            }
+        }
+}
