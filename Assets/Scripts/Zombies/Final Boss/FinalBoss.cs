c7634ecc24bcd6a73d1940d0521dd49ba04932c6
ahmedshams99 <ahmed.awadshams@student.guc.edu.eg>
2021-01-14 01:42:40 +0200

    Final Boss

---
+++ Assets/Scripts/Zombies/Final Boss/FinalBoss.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBoss : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject meteor;
+    bool canAttack;
+    public int health = 10000;
+    float coolDownTime = 5f;
+    void Start()
+    {
+        StartCoroutine(coolDown());
+    }
+    void Update()
+    {
+        if(canAttack && health>0)
+        {
+            // if(Random.Range(0.0f, 1.0f)>0.5)
+            //     StartCoroutine(earthquake());
+            // else 
+            //     StartCoroutine(spawnZombies());
+            StartCoroutine(spawnZombies());
+        }
+    }
+    IEnumerator spawnZombies()
+    {
+        canAttack = false;
+        animator.SetTrigger("Throw");
+        yield return new WaitForSeconds(160.0f/30.0f);
+        meteor.GetComponent<MeteorScript>().ThrowMeteor();
+        StartCoroutine(coolDown());
+    }
+    IEnumerator earthquake()
+    {
+        canAttack = false;
+        animator.SetTrigger("Earthquake");
+        yield return new WaitForSeconds(55.0f/30.0f);
+        for(int i=0;i<10;i++)
+        {
+            if(PlayerController.instance.isGrounded)
+            {
+                PlayerController.instance.TakeDamage(50);
+                break;
+            }
+            yield return 0;
+        }
+        //If player is on ground 
+            //Take Damage
+        StartCoroutine(coolDown());
+    }
+    IEnumerator coolDown()
+    {
+        yield return new WaitForSeconds(coolDownTime);
+        canAttack = true;
+    }
+    public void TakeDamage(int damage)
+    {
+        health-=damage;
+        if(health<=0)
+            Die();
+    }
+    void Die()
+    {
+        animator.SetTrigger("Dead");
+    }
+}
---
+++ Assets/Scripts/Zombies/Final Boss/MeteorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject horde;
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameObject.SetActive(false);
+    }
+    public void ThrowMeteor()
+    {
+        gameObject.SetActive(true);
+        rb.useGravity = true;
+        Vector3 dir = PlayerController.instance.player.transform.position - transform.position;
+        // dir.y=0;
+        float distance = dir.magnitude;
+        rb.AddForce(dir,ForceMode.Impulse);
+        // rb.AddTorque(new Vector3(10, 0, 10));
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);
+        if(LayerMask.LayerToName(other.gameObject.layer) == "Player")
+        {
+            PlayerController.instance.TakeDamage(50);
+        }
+        else if (LayerMask.LayerToName(other.gameObject.layer) == "World")
+        {
+            GameObject myHorde = Instantiate(horde);
+            myHorde.transform.position = transform.position;
+        }
+    }
+}
