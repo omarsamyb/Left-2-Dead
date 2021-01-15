dde4a7846c4ef96473347dab6066f87f438ab5db
omarsamyb <omarsamyb@gmail.com>
2021-01-15 04:59:52 +0200

    main menu

---
+++ Assets/Scripts/MainMenu/PoseAnimator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PoseAnimator : MonoBehaviour
+{
+    private Animator poseAnimator;
+    private float poseTimeRef = 10f;
+    private float poseTime;
+
+    void Start()
+    {
+        poseAnimator = GetComponent<Animator>();
+        poseTime = 10f;
+    }
+
+    void Update()
+    {
+        if (poseAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Pose"))
+        {
+            poseTime -= Time.deltaTime;
+            if (poseTime <= 0f)
+            {
+                poseTime = poseTimeRef;
+                poseAnimator.SetTrigger("switchPose");
+            }
+        }
+    }
+}
