16e47f1b0463c8619118e8be344f78443703177d
B1aCkManTa <mohamed.mantawy@student.guc.edu.eg>
2021-01-15 19:53:39 +0200

    level 3 polishing
    Co-Authored-By: ahmedalaaseif <47136510+ahmedalaaseif@users.noreply.github.com>
    Co-Authored-By: Ahmed Shams <ahmed.s001999@gmail.com>

--- Assets/Prefabs/Maps/FinalRoom.cs
+++ Assets/Prefabs/Maps/FinalRoom.cs
@@ -6,16 +6,28 @@ public class FinalRoom : MonoBehaviour
 {
     public GameObject barrier;
     public GameObject finalBoss;
+    public GameObject boom;
     bool spawned = false;
+    void Start(){
+        boom.GetComponent<ParticleSystem>().Stop();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")&&!spawned)
         {
             spawned = true;
+            StartCoroutine(playPS());
             Destroy(barrier);
             Instantiate(finalBoss);
             GameManager.instance.timerIsRunning = false;
             GameManager.instance.failedRescue = false;
         }
+    }
+    IEnumerator playPS(){
+        boom.SetActive(true);
+        boom.GetComponent<ParticleSystem>().Play();
+        yield return new WaitForSeconds(1.5f);
+        boom.GetComponent<ParticleSystem>().Stop();
+        boom.SetActive(false);
     }
 }
